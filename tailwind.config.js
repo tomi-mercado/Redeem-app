48455f4b4c99408b09ddeaa8a9bb38713038dc4d
tomi-mercado <tmercadoslp@gmail.com>
2021-12-03 17:15:25 -0300

    (tailwind.config): replace shadow-md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  purge: ["./components/**/*.tsx", "./pages/**/*.tsx", "./public/**/*.html"],
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
   darkMode: false,
   theme: {
     extend: {
       colors: {
         blue: {
-          200: '#0AD4FA'
-        }
-      }
+          200: '#0AD4FA',
+        },
+      },
+      boxShadow: {
+        md: '2px 2px 4px 0 rgba(0,0,0,0.10);',
+      },
     },
   },
   variants: {
