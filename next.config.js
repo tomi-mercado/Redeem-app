67f567ff9490990f02670b8f84826e110fe9a6e7
tomi-mercado <tmercadoslp@gmail.com>
2021-12-03 17:14:47 -0300

    (next.config.js): add images domains

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
     AEROLAB_CHALLENGE_TOKEN: process.env.AEROLAB_CHALLENGE_TOKEN,
     API_URL: process.env.API_URL,
   },
+  images: {
+    domains: ['coding-challenge-api.aerolab.co'],
+  },
 };
