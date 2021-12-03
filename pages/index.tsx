3a7a26457c85eb4ac3e54b5b947acf31c425808a
tomi-mercado <tmercadoslp@gmail.com>
2021-12-02 23:22:04 -0300

    (pages/index): implement Navbar make userData req

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>Bienvenido</div>;
+import { GetStaticProps } from "next";
+import { staticPropsRequest } from "../common/functions";
+import { UserData } from "../common/interfaces";
+import Navbar from "../components/Navbar";
+import { getUserData } from "../services/aerolabRedeem.service";
+
+export default function Home({
+  userData,
+  error,
+}: {
+  userData?: UserData;
+  error?: boolean | object;
+}) {
+  if (!userData && !error) {
+    // TODO: spinner
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    // TODO: error component
+    return <div>Error: Failed to load data</div>;
+  }
+  return <Navbar userName={userData?.name} userPoints={userData?.points} />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const result = await staticPropsRequest<UserData>("userData", getUserData);
+  return result;
+};
