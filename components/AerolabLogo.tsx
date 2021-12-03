f826924761d30e3dd1fad4d4952879ca0f4f7385
tomi-mercado <tmercadoslp@gmail.com>
2021-12-02 23:15:48 -0300

    (components): add AerolabLogo & Coin

---
+++ components/AerolabLogo.tsx
@@ -0,0 +1,10 @@
+import ImageComponentFactory from "../factories/ImageComponentFactory";
+
+import aerolabLogo from "../images/aerolab-logo.svg";
+
+const AerolabLogo = ImageComponentFactory({
+  src: aerolabLogo,
+  alt: "Aerolab Logo",
+});
+
+export default AerolabLogo;
