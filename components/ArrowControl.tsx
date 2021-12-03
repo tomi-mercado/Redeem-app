b5102c109aafa26788621b8677671c33e0ccbe42
tomi-mercado <tmercadoslp@gmail.com>
2021-12-03 16:00:01 -0300

    (components/ArrowControl): add props onClickNext and onClickPrev

--- components/ArrowControl.tsx
+++ components/ArrowControl.tsx
@@ -5,13 +5,24 @@ const { ArrowLeft, ArrowRight } = arrows;
 export interface ArrowControlProps {
   page: number;
   pages: number;
+  onClickNext?: () => void;
+  onClickPrev?: () => void;
 }
 
-export default function ArrowControl({ page, pages }: ArrowControlProps) {
+export default function ArrowControl({
+  page,
+  pages,
+  onClickNext,
+  onClickPrev,
+}: ArrowControlProps) {
   return (
     <div className="flex items-center justify-between w-28">
-      {page > 1 && <ArrowLeft className="cursor-pointer" />}
-      {page < pages && <ArrowRight className="cursor-pointer" />}
+      {page > 1 && (
+        <ArrowLeft onClick={onClickPrev} className="cursor-pointer" />
+      )}
+      {page < pages && (
+        <ArrowRight onClick={onClickNext} className="cursor-pointer" />
+      )}
     </div>
   );
 }
