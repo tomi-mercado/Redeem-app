580d5605a082b3f3f34f8fd947c218f24587aa09
tomi-mercado <tmercadoslp@gmail.com>
2021-12-03 15:58:47 -0300

    (components/CatalogueOptions): implement ArrowControl

--- components/CatalogueOptions.tsx
+++ components/CatalogueOptions.tsx
@@ -1,25 +1,34 @@
+import ArrowControl, { ArrowControlProps } from './ArrowControl';
 import RenderProductsCounter, {
   RenderProductsCounterProps,
 } from './RenderProductsCounter';
 import Sorter, { SorterProps } from './Sorter';
+import arrows from './arrows';
+
+const { ArrowLeft, ArrowRight } = arrows;
 
 export interface CatalogueOptionsProps {
   renderProductsCounter: RenderProductsCounterProps;
   sorter: SorterProps;
+  arrowControl: ArrowControlProps;
 }
 
 export default function CatalogueOptions({
   renderProductsCounter,
   sorter,
+  arrowControl,
 }: CatalogueOptionsProps) {
   return (
-    <div className="flex w-full items-center divide-x">
-      <div className="mr-6">
-        <RenderProductsCounter {...renderProductsCounter} />
+    <div className="flex w-full items-center justify-between">
+      <div className="flex divide-x items-center">
+        <div className="mr-6">
+          <RenderProductsCounter {...renderProductsCounter} />
+        </div>
+        <div className="pl-6">
+          <Sorter {...sorter} />
+        </div>
       </div>
-      <div className="pl-6">
-        <Sorter {...sorter} />
-      </div>
+      <ArrowControl {...arrowControl} />
     </div>
   );
 }
