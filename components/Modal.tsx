afbbf8b66f5b5c373424379125b4f4d746f45f8e
tomi-mercado <tmercadoslp@gmail.com>
2021-12-06 13:32:42 -0300

    (components/Modal): add props open & onClose

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from 'react';
+
 export interface ModalProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  open?: boolean;
+  onClose?: () => void;
 }
 
-export default function Modal({ children }: ModalProps) {
+export default function Modal({ children, open, onClose }: ModalProps) {
+  const [localOpen, setLocalOpen] = useState(false);
+
+  useEffect(() => {
+    setLocalOpen(open || false);
+  }, [open]);
+
   return (
     <div
-      className="fixed z-10 inset-0 overflow-y-auto"
+      className={`fixed z-10 inset-0 overflow-y-auto transition-opacity ${
+        localOpen ? 'opacity-100' : 'opacity-0 hidden'
+      }`}
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
+      onClick={onClose}
     >
       <div className="items-end justify-center min-h-screen flex flex-column items-center justify-center p-0 sm:pt-4 sm:px-4 sm:pb-20 text-center block p-0">
         <div
-          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          className={`fixed inset-0 bg-gray-500 bg-opacity-75`}
           aria-hidden="true"
         ></div>
 
