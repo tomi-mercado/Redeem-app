9e194ef70cc4e930959e34313244905020e64169
tomi-mercado <tmercadoslp@gmail.com>
2021-12-09 08:06:06 -0300

    (components/arrows): add ArrowDown

--- components/arrows.tsx
+++ components/arrows.tsx
@@ -1,4 +1,4 @@
-import { HtmlHTMLAttributes, SVGProps } from 'react';
+import { SVGProps } from 'react';
 
 const arrows = {
   ArrowRight: (props: SVGProps<SVGSVGElement>) => (
@@ -83,6 +83,52 @@ const arrows = {
               </g>
             </g>
           </g>
+        </g>
+      </g>
+    </svg>
+  ),
+  ArrowDown: (props: SVGProps<SVGSVGElement>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      width="256"
+      height="256"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <desc>Created with Fabric.js 1.7.22</desc>
+      <defs></defs>
+      <g transform="translate(128 128) scale(0.72 0.72)">
+        <g
+          style={{
+            stroke: 'none',
+            strokeWidth: 0,
+            strokeDasharray: 'none',
+            strokeLinecap: 'butt',
+            strokeLinejoin: 'miter',
+            strokeMiterlimit: 10,
+            fill: 'none',
+            fillRule: 'nonzero',
+            opacity: 1,
+          }}
+          transform="translate(-175.05 -175.05000000000004) scale(3.89 3.89)"
+        >
+          <path
+            d="M 90 24.25 c 0 -0.896 -0.342 -1.792 -1.025 -2.475 c -1.366 -1.367 -3.583 -1.367 -4.949 0 L 45 60.8 L 5.975 21.775 c -1.367 -1.367 -3.583 -1.367 -4.95 0 c -1.366 1.367 -1.366 3.583 0 4.95 l 41.5 41.5 c 1.366 1.367 3.583 1.367 4.949 0 l 41.5 -41.5 C 89.658 26.042 90 25.146 90 24.25 z"
+            style={{
+              stroke: 'currentColor',
+              strokeWidth: 0,
+              strokeDasharray: 'none',
+              strokeLinecap: 'butt',
+              strokeLinejoin: 'miter',
+              strokeMiterlimit: 10,
+              fill: 'rgb(0,0,0)',
+              fillRule: 'nonzero',
+              opacity: 1,
+            }}
+            transform=" matrix(1 0 0 1 0 0) "
+            stroke-linecap="round"
+          />
         </g>
       </g>
     </svg>
